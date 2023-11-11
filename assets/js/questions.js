955dd2bafc46b354369f24af926fa3d090489c3f
nicholasholder <ngholder@hotmail.com>
2023-11-11 17:40:11 -0600

    added assets and index.html

---
+++ assets/js/questions.js
@@ -0,0 +1,29 @@
+var questions = [
+    {
+        text: 'Question One',
+        choices: ['answer one', 'answer two'],
+        answer: 'answer one'
+    },
+    {
+        text: 'Question One',
+        choices: ['answer one', 'answer two'],
+        answer: 'answer one'
+    },
+     {
+        text: 'Question One',
+        choices: ['answer one', 'answer two'],
+        answer: 'answer one'
+    },
+    {
+        text: 'Question One',
+        choices: ['answer one', 'answer two'],
+        answer: 'answer one'
+    },
+    {
+        text: 'Question One',
+        choices: ['answer one', 'answer two'],
+        answer: 'answer one'
+    },
+
+
+]
